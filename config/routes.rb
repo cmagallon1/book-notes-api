8b388276ede20daf20eebcd178bf98fa862196b5
cmagallon1 <cesar9mv@gmail.com>
2020-03-06 10:50:42 -0600

    [add] models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  post 'users/create'
+  get 'users/show'
+  post 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
