1753a15328a4d293702186b4f16ac1a6855a3d8b
cmagallon1 <cesar9mv@gmail.com>
2020-03-09 01:05:44 -0600

    [add] jwt

---
+++ lib/web_token.rb
@@ -0,0 +1,13 @@
+class WebToken
+  SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+  def self.encode(payload, exp = 24.hours.from_now.to_i)
+    payload[:exp] = exp
+    JWT.encode payload, SECRET_KEY, 'HS256'
+  end 
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: 'HS256' })[0]
+    HashWithIndifferentAccess.new(decoded)
+  end
+end
