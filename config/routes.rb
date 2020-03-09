a937037587865f5cbad52dddae97335349d90daa
cmagallon1 <cesar9mv@gmail.com>
2020-03-09 01:05:01 -0600

    [add] configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/users' do
+    post '/signup', to: 'users#signup'
+
+    post '/signin', to: 'users#signin'
+
+    post '/signout', to: 'users#signout'
+
+    put '/update/:id', to: 'users#update'
+
+    delete '/destroy/:id', to: 'users#destroy'
+  end
+
 end
