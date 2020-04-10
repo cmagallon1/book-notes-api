38bb2c4ad38e150767086c8439ab377ebf23404e
cmagallon1 <cesar9mv@gmail.com>
2020-04-09 22:27:43 -0500

    [REF] models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
   enum status: { awaiting: 0, finished: 1, wip: 2 } 
-  validates :name, presence: true
-  validates :author, presence: true
-  validates :status, presence: true
+  validates :author, :status, :category, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :category, presence: true
 
-  before_save do
+  scope :books, -> (filters){ where("#{filters[:field]} = ?", filters[:value]) }
+
+  before_validation(on: :create) do
     self.status = 0
   end
 end
